87b143370313ef9e67db208886623c284c0e8595
xinyuan-zhao <xinyuan7@ualberta.ca>
2021-10-30 01:14:54 -0600

    map and photo done

--- app/src/main/java/com/example/habittracker/HabitEventEditActivity.java
+++ app/src/main/java/com/example/habittracker/HabitEventEditActivity.java
@@ -5,14 +5,22 @@ import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContract;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.text.Html;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -25,6 +33,10 @@ import android.widget.Toast;
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.libraries.places.api.Places;
 import com.google.android.libraries.places.api.model.Place;
 import com.google.android.libraries.places.api.net.PlacesClient;
@@ -35,13 +47,17 @@ import com.google.android.libraries.places.widget.model.AutocompleteActivityMode
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 
 public class HabitEventEditActivity extends AppCompatActivity implements DeleteConfirmFragment.OnDeleteConfirmFragmentInteractionListener {
     EditText location_editText;
     Button photo_button;
     ImageView photo_imageView;
     ActivityResultLauncher<Intent> activityResultLauncher;
-    ActivityResultLauncher<Intent> activityResultLauncher2;
+
+    Button currentLocation_button;
+    FusedLocationProviderClient fusedLocationProviderClient;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -83,6 +99,7 @@ public class HabitEventEditActivity extends AppCompatActivity implements DeleteC
 
 
 // photo part
+// reference: https://www.youtube.com/watch?v=HxlAktedIhM
         photo_button = findViewById(R.id.habitEvent_addPhoto_button);
         photo_imageView = findViewById(R.id.habitEvent_photo_imageView);
 
@@ -143,11 +160,59 @@ public class HabitEventEditActivity extends AppCompatActivity implements DeleteC
 
 
     // Reference: https://www.youtube.com/watch?v=t8nGh4gN1Q0
+//        and https://www.youtube.com/watch?v=qO3FFuBrT2E for onActivityResult is Deprecated
         // Implement Autocomplete Place Api
 
         // initialize place
         Places.initialize(getApplicationContext(),"AIzaSyCJvvbjw-Qdfxe_fwAnE9HwVFE9SelWUP0");
         PlacesClient placesClient = Places.createClient(this);
+
+
+        // get current location https://www.youtube.com/watch?v=Ak1O9Gip-pg
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        currentLocation_button = findViewById(R.id.habitEvent_currentLocation_button);
+        currentLocation_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // check permission
+                if(ActivityCompat.checkSelfPermission(HabitEventEditActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED) {
+                    // when permission granted
+
+                    fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Location> task) {
+                            // initialize location
+                            Location location = task.getResult();
+                            if(location != null){
+                                try{
+                                    // initialize getCoder
+                                    Geocoder geocoder = new Geocoder(HabitEventEditActivity.this, Locale.getDefault());
+                                    // initialize address list
+                                    List<Address> addresses = geocoder.getFromLocation(
+                                            location.getLatitude(),location.getLongitude(),1
+
+                                    );
+                                    location_editText.setText(Html.fromHtml(addresses.get(0).getAddressLine(0)));
+                                } catch(IOException e1){
+                                    e1.printStackTrace();
+                                }
+
+                            }
+                        }
+                    });
+                }else{
+                    // when permission denied
+                    ActivityCompat.requestPermissions(HabitEventEditActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+                }
+            }
+        });
+
+
+
+
+
+
+
 
         activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
             @Override
