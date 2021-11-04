2d91b6d7ab8b8da735160d5a1e5ced8c906d9550
Kevin Tang <kaiwen2@ualberta.ca>
2021-11-04 10:09:36 -0600

    Modify profile unit test so all test works

--- app/src/test/java/com/example/habittracker/HabitEventTest.java
+++ app/src/test/java/com/example/habittracker/HabitEventTest.java
@@ -1,4 +1,9 @@
 package com.example.habittracker;
 
+import org.junit.Before;
+import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertTrue;
+
 public class HabitEventTest {
 }
