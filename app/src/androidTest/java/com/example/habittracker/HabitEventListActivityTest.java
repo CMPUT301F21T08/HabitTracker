ddd8a61531013085a986fd2581add47af937119e
TerryOne <55670231+Terry-One@users.noreply.github.com>
2021-11-05 15:52:39 -0600

    Update HabitEventListActivityTest.java

--- app/src/androidTest/java/com/example/habittracker/HabitEventListActivityTest.java
+++ app/src/androidTest/java/com/example/habittracker/HabitEventListActivityTest.java
@@ -2,7 +2,9 @@ package com.example.habittracker;
 
 import android.app.Activity;
 import android.widget.EditText;
+import android.widget.ListView;
 
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 import com.robotium.solo.Solo;
@@ -11,11 +13,12 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
  * Test class for Habit Event List Activity
  */
-
+@RunWith(AndroidJUnit4.class)
 public class HabitEventListActivityTest {
     private Solo solo;
 
@@ -30,7 +33,7 @@ public class HabitEventListActivityTest {
 
     @Before
     public void setUp() throws Exception{
-        solo = new Solo(InstrumentationRegistry.getInstrumentation().rule.getActivity());
+        solo = new Solo(InstrumentationRegistry.getInstrumentation(), rule.getActivity());
     }
 
     /**
@@ -49,28 +52,17 @@ public class HabitEventListActivityTest {
      */
     @Test
     public void checkList() {
-        //Asserts that current activity is the LogInActivity
-        // because that is where we start
-        solo.assertCurrentActivity("Wrong Activity", LogInActivity.class);
-        solo.enterText((EditText) solo.getView(R.id.login_useremail_editText), "test@gmail.com");
-        solo.enterText((EditText) solo.getView(R.id.login_password_editText), "123456");
-        solo.clickOnButton("Sign In");
-        //After logIn we go to the Main Page
-        //Here we check if we are on the Main Page Activity
-        solo.assertCurrentActivity("Wrong Activity", MainPageActivity.class);
-        //From Main Page, we need to click on Habits events
-        // So that we can see all habit events
-        solo.clickOnButton("Habit Event");
-
-        //User can delete a habit by clicking on the icon
-        ////if user click the delete button
-        //a dialog pops out
-        //check the dialog
-
-        solo.clickOnButton("Delete");
-        solo.clickOnView(solo.getView(R.id.tv_delete_confirm);
+        //Asserts that current activity is the HabitEventListActivity
+        solo.assertCurrentActivity("Wrong Activity", HabitEventListActivity.class);
 
 
+        //assert in correct Activity
+        HabitEventListActivity activity = (HabitEventListActivity) solo.getCurrentActivity();
+        //get listView
+        ListView listView = activity.findViewById(R.id.lv_habit_event);
+        HabitEvent newHabE = (HabitEvent) listView.getItemAtPosition(0);// only habit in the one
+        // check that the habit Event is correctly listed
+        if ((!newHabE.getEventTitle().equals("New: 11-05-2021"))) throw new AssertionError();
     }
 
     /**
