ca10e7ac3e8affa70c19abd585074225ba71d135
Kevin Tang <kaiwen2@ualberta.ca>
2021-11-22 19:24:44 -0700

    Solve the problem that the 'notDone' attribute keeps changing its value when we delete a habit event

--- app/src/main/java/com/example/habittracker/MainPageActivity.java
+++ app/src/main/java/com/example/habittracker/MainPageActivity.java
@@ -121,7 +121,7 @@ public class MainPageActivity extends AppCompatActivity {
                             }
                             break;
                     }
-                    // upload the information to database to update all habit
+//                     upload the information to database to update all habit
                     HashMap<String, Object> map = new HashMap<>();
                     map.put(habit.getUUID(),habit);
                     FirebaseDatabase.getInstance().getReference().child(uid).child("Habit").updateChildren(map);
