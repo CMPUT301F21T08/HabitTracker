78c4398eeec3d1eec010309d76b9c4c7af939c9f
ingabire19 <77806210+ingabire19@users.noreply.github.com>
2021-11-29 14:03:29 -0700

    Add files via upload

---
+++ app/src/main/java/com/example/habittracker/listener/HabitDescriptionFrequencyListener.java
@@ -0,0 +1,29 @@
+package com.example.habittracker.listener;
+
+import android.view.View;
+
+import androidx.fragment.app.FragmentManager;
+
+import com.example.habittracker.Habit;
+import com.example.habittracker.ShowWeekDaysFragment;
+
+public class HabitDescriptionFrequencyListener implements View.OnClickListener{
+    //Attributes values
+    FragmentManager fragmentManager;
+    Habit habit;
+
+    public HabitDescriptionFrequencyListener(FragmentManager fragmentManager, Habit habit){
+        this.fragmentManager = fragmentManager;
+        this.habit = habit;
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        // if the frequencyType is "per week", then invoke a fragment to show the occurrence week day of the habit
+        if(habit.getFrequencyType().equals("per week")){
+            new ShowWeekDaysFragment(habit).show(fragmentManager,"SHOW_OCCURRENCE_DAYS");
+        }
+    }
+}
